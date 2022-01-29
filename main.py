5dc37508ccfe747c8d510219f78168f6829ac410
websitetest987 <john.winfer88@gmail.com>
2022-01-29 21:40:14 +0300

    Least sqaure method and trapezoid rule

--- main.py
+++ main.py
@@ -1,46 +1,145 @@
+
 import math
 import os
 
-x = [1,3,5,7,9,11,13,15]
-y = [60,35.2,16.7,10.93,7.12,4.45,2.05,0]
-aMatrix = []
-xMatrix = []
-row2 = []
 
-row3 = []
-num = 0
+from matplotlib.pyplot import step
 
-for expon in range(5):
-    num = 0
-    for i in x:
-        num = num + math.pow(i, expon)
-    xMatrix.append(round(num, 10))
+passC = 0
 
-num = 0
-num1 = 0
-count = 0
+while True:
+    error = True
 
-for i in x:
-    num = num + math.pow(i, 2) * y[count] #need to find a diffrent way to do this.
-    num1 = num1 + i * y[count]
-    count = count + 1
+    if passC == 4:
+        print("Please insert step length:")
+        sLength = input()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        try:
+            sLength = float(sLength)
+            error = False
+        except:
+            print("Invalid Input")
+            error = True
 
-aMatrix.extend((round(num, 10), round(num1, 10), round(sum(y), 10))) 
+    
+    if passC == 3:
+        passC = 4
+        print("Please insert upperbound:")
+        uBound = input()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        try:
+            uBound = float(uBound)
+        except:
+            print("Invalid Input")
+            passc = 3
 
-num = 0
-for i in range(2):
-    xMatrix.append(round(xMatrix[i + 2] - (xMatrix[1] / xMatrix[2]) * xMatrix[i + 3], 10))
+    if passC == 2:
+        passC = 3
+        print("Please insert lowerbound:")
+        lBound = input()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        try:
+            lBound = float(lBound)
+        except:
+            print("Invalid Input")
+            passC = 2
+            
+    if passC == 1:
+        passC = 2
+        print("Please insert Y values seperated by commas:")
+        userInput = input()
+        y1 = userInput.split(",")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for i in range(0, len(y1)):
+            try:
+                y1[i] = float(y1[i])
+            except:
+                print("Invalid Input")
+                passC = 1
 
-aMatrix[1] = round(aMatrix[1] - (xMatrix[1] / xMatrix[2]) * aMatrix[0], 10)
-           
-print(xMatrix)
+    
+    if passC == 0 :
+        passC = 1
+        print("Please insert X values seperated by commas:")
+        userInput = input()
+        x1 = userInput.split(",")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for i in range(0, len(x1)):
+            try:
+                x1[i] = float(x1[i])
+            except:
+                print("Invalid Input")
+                passC = 0
+    
+    x = [1,3,5,7,9,11,13,15]
+    y = [60,35.2,16.7,10.93,7.12,4.45,2.05,0]
+    aMatrix = []
+    xMatrix = []
+    
+    if error == False:
+        num = 0
 
-print (aMatrix)
+        for expon in range(5):
+            num = 0
+            for i in x:
+                num = num + math.pow(i, expon)
+            xMatrix.append(round(num, 10))
 
-a = (round((aMatrix[2] - (xMatrix[0]/xMatrix[2]) * aMatrix[0]) - ((xMatrix[1] - (xMatrix[0] / xMatrix[2]) * xMatrix[3]) / xMatrix[5]) * aMatrix[1], 10)) / round((xMatrix[2] - (xMatrix[0]/xMatrix[2]) * xMatrix[4]) - ((xMatrix[1] - (xMatrix[0] / xMatrix[2]) * xMatrix[3]) / xMatrix[5]) * xMatrix[6], 10)
-b = (aMatrix[1] - a * xMatrix[6]) / xMatrix[5]
-c = (aMatrix[0] - (xMatrix[4] * a) - (xMatrix[3] * b)) / xMatrix[2]
-print(a)
-print(b)
-print(c)
+        num = 0
+        num1 = 0
+        count = 0
+
+        for i in x:
+            num = num + math.pow(i, 2) * y[count] 
+            num1 = num1 + i * y[count]
+            count = count + 1
+
+        aMatrix.extend((round(num, 10), round(num1, 10), round(sum(y), 10))) 
+
+        num = 0
+        for i in range(2):
+            xMatrix.append(round(xMatrix[i + 2] - (xMatrix[1] / xMatrix[2]) * xMatrix[i + 3], 10))
+
+        aMatrix[1] = round(aMatrix[1] - (xMatrix[1] / xMatrix[2]) * aMatrix[0], 10)
+                
+
+
+        a = (round((aMatrix[2] - (xMatrix[0]/xMatrix[2]) * aMatrix[0]) - ((xMatrix[1] - (xMatrix[0] / xMatrix[2]) * xMatrix[3]) / xMatrix[5]) * aMatrix[1], 10)) / round((xMatrix[2] - (xMatrix[0]/xMatrix[2]) * xMatrix[4]) - ((xMatrix[1] - (xMatrix[0] / xMatrix[2]) * xMatrix[3]) / xMatrix[5]) * xMatrix[6], 10) 
+        b = (aMatrix[1] - a * xMatrix[6]) / xMatrix[5]
+        c = (aMatrix[0] - (xMatrix[4] * a) - (xMatrix[3] * b)) / xMatrix[2]
+
+        dist = (uBound - lBound) / sLength
+
+        try:
+            dist = int(dist)
+            extra = 0
+
+        except:
+            extra = round(dist % 1, 10) 
+            math.ceil(dist)
+            dist = int(dist)
+
+        
+        dist1 = dist + sLength
+        num = 0
+        
+        xn = lBound + sLength
+        print(xn)
+        dist = dist - 2
+        for i in range(dist):
+            num = num + a*math.pow(xn, 2) + b * xn + c
+            xn = xn + sLength
+        area2D = 0.5 * (sLength*((a*math.pow(lBound, 2) + b * lBound + c) + (a*math.pow(uBound, 2) + b * uBound + c) + (2 * num)))
+        
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Quadratic Equation is:", round(a, 5),"x^2",round(b, 5),"x",round(c, 5))
+        print ("2D area under the shape is:", round(area2D,5))
+        
+        
+        passC = 0
+        asas = input()
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
 
