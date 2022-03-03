b16fee6ab2c6a1bc5fe6d90af1b8d1ba5e38203c
JohnWinfer <60031333+JohnWinfer@users.noreply.github.com>
2022-03-03 22:05:19 +0300

    Update 3D method.py

--- 3D method.py
+++ 3D method.py
@@ -2,15 +2,17 @@
 
 import math
 import os
+from turtle import width
+
+from numpy import angle
 
 
-Height3D = 1
-width3D = 0.1
-stepHeight = 1
+
+angle3D = 0.1
+stepHeight = 0.5
 a = 0.375
 b = -0.5
 c = 2.125
-
 #a = -1.2
 #b = 5.5
 #c = -1.33
@@ -18,10 +20,10 @@ c = 2.125
 Area2D = 387.71588
 lbound = 1
 ubound = 5
-highestValue = 79
 
-num = highestValue / Height3D
-num = math.floor(num)
+
+
+
 
 bruteforce = True
 xValue = lbound
@@ -34,7 +36,7 @@ while bruteforce == True:
         break
     yValue = a * math.pow(xValue, 2) + b * xValue + c
     yValue = round(yValue, 3)
-    if yValue % stepHeight == 0 and yValue != testyValue:
+    if yValue % stepHeight == 0 and yValue != testyValue and yValue >= 0:
         yValues3D.append(yValue)
         xValues3D.append(xValue)
     xValue = round(xValue + .0001, 10)
@@ -52,29 +54,53 @@ for i in yValues3D:
 yValue = 0
 total = 0
 waitingList = []
+waitingListY = []
 
-for i in range(num):
-    yValue + stepHeight
-    if yValue is not yValues3D:
-        dist = ubound - lbound
-    else:
-        dist = xValue - lbound
-    height = math.sqrt(math.pow((dist), 2) - math.pow(width3D / 2, 2))
-    base = math.sqrt(math.pow(dist, 2) - math.pow(height, 2))
-    area = (base * height) * 2
-    if yValue in duplicates:
-        waitingList.append(area)
-        waitingList.append(yValue)
-    else:
-        total = total + area
+angle3D = angle3D / 2
+fltrigA = min(yValues3D) / stepHeight
+
+height = ubound - lbound
+
+angleB = 90 - angle3D
+
+width = height * (math.sin(angle3D * 0.01745) / math.sin(angleB * 0.01745))
+fltrig = ((height * width) / 2) * stepHeight
+total = fltrig * fltrigA
+count = 0
+print(total)
+for i in xValues3D:
+    height = i - lbound
+    width =  height * (math.sin(angle3D * 0.01745) / math.sin(angleB * 0.01745))
+    trig = (((width * height) / 2) * stepHeight)
+    if yValues3D[count] in duplicates:
+        if yValues3D[count] in waitingListY:
+            count1 = 0
+            for z in waitingListY:
+                if z == yValues3D[count]:
+                    saved = count1
+                    break
+                else:
+                    count1 = count1 + 1
+            if a >= 0:
+                trig = fltrig - (trig - waitingList[saved])
+            else:
+                trig = trig - waitingList[saved]
+        else:
+            waitingList.append(trig)
+            waitingListY.append(yValues3D[count])
+    
+    total = total + trig
+    count = count + 1
+total = total * (360 / angle3D)
 
 
+print(total)
 
 print(xValues3D)
 print(yValues3D)
 print(duplicates)
 
-print(area)
+
 
 #Notes:
 # duplicates get subtraceted from each other
